b8517338191b6c47b6c6b87f8be711c06cb8ad19
Emir Alp <Emir Alp@EMIR-ALP>
2025-10-03 18:10:31 +0300

    Entities Done!

---
+++ Cental.EntityLayer/Entities/CarCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cental.EntityLayer.Entities
+{
+    public class CarCategory
+    {
+        public int CarCategoriesId { get; set; }
+        public string CategoryName { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
