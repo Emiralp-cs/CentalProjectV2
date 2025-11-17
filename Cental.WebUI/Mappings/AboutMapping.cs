18c6edafb289f0bc156096f49b0f4267511e7f9c
Emir Alp Esmergül <emiralpce35@gmail.com>
2025-11-17 21:22:20 +0300

    abcd

---
+++ Cental.WebUI/Mappings/AboutMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Cental.DataTransferObjectLayer.Dtos.AboutDtos;
+using Cental.EntityLayer.Entities;
+
+namespace Cental.WebUI.Mappings
+{
+    public class AboutMapping : Profile
+    {
+        public AboutMapping()
+        {
+            CreateMap<About, ToListAboutDto>().ReverseMap();
+            
+        }
+    }
+}
